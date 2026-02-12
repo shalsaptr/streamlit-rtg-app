ec1f38e4fcf941ac6ef8a3fc6a98aca0a38582a9
shalsaptr <shalsaputriiii@gmail.com>
2026-02-12 11:16:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,32 @@ files = st.file_uploader(
 
 all_df = []
 
+def read_excel_safe(file):
+    name = file.name.lower()
+    if name.endswith(".xlsx"):
+        return pd.read_excel(file, engine="openpyxl")
+    elif name.endswith(".xls"):
+        return pd.read_excel(file, engine="xlrd")
+    else:
+        return pd.read_excel(file)
+
 if files:
     for file in files:
-        df = pd.read_excel(file)
-        bulan = ".".join(file.name.split(".")[:2])
-        df.insert(0, "BULAN", bulan)
-        all_df.append(df)
+        try:
+            df = read_excel_safe(file)
 
-    final = pd.concat(all_df, ignore_index=True)
-    st.dataframe(final)
+            bulan = ".".join(file.name.split(".")[:2])
+            df.insert(0, "BULAN", bulan)
 
-    final.to_excel("hasil.xlsx", index=False)
-    with open("hasil.xlsx","rb") as f:
-        st.download_button("Download Excel", f, "hasil.xlsx")
+            all_df.append(df)
+
+        except Exception as e:
+            st.error(f"Gagal baca file {file.name}: {e}")
+
+    if all_df:
+        final = pd.concat(all_df, ignore_index=True)
+        st.dataframe(final)
+
+        final.to_excel("hasil.xlsx", index=False)
+        with open("hasil.xlsx","rb") as f:
+            st.download_button("Download Excel", f, "hasil.xlsx")
